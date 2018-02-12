de6d792ca5f4d66bb8e23fa7bbd3b2d5ba875e23
nguyenthecuong <1994nguyenthecuong1994@gmail.com>
2018-02-12 14:35:56 +0700

    Title Case a Sentence

---
+++ javascript-5/javascript.ts
@@ -0,0 +1,11 @@
+function titleCase(str: string): string  {
+  
+    let convertToArray = str.toLowerCase().split(" ");
+    let result = convertToArray.map(function(val){
+        return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+    });
+    return result.join(" ");
+  }
+  
+  titleCase("I'm a little tea pot");
+  
